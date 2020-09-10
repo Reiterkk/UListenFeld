19748ed03959866b78c75df481dc14daf3bf935d
Kevin Reiter <kevin.reiter@kit.edu>
2020-09-10 16:27:54 +0200

    Austausch der gewählten Listitems in die jeweils andere Liste implementiert.

--- UListenFeld/Form1.Designer.cs
+++ UListenFeld/Form1.Designer.cs
@@ -28,13 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LstLinks = new System.Windows.Forms.ListBox();
+            this.LstRechts = new System.Windows.Forms.ListBox();
+            this.CmdRechts = new System.Windows.Forms.Button();
+            this.CmdLinks = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LstLinks
+            // 
+            this.LstLinks.FormattingEnabled = true;
+            this.LstLinks.ItemHeight = 32;
+            this.LstLinks.Location = new System.Drawing.Point(12, 12);
+            this.LstLinks.Name = "LstLinks";
+            this.LstLinks.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.LstLinks.Size = new System.Drawing.Size(240, 420);
+            this.LstLinks.TabIndex = 0;
+            // 
+            // LstRechts
+            // 
+            this.LstRechts.FormattingEnabled = true;
+            this.LstRechts.ItemHeight = 32;
+            this.LstRechts.Location = new System.Drawing.Point(403, 12);
+            this.LstRechts.Name = "LstRechts";
+            this.LstRechts.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.LstRechts.Size = new System.Drawing.Size(240, 420);
+            this.LstRechts.TabIndex = 1;
+            // 
+            // CmdRechts
+            // 
+            this.CmdRechts.Location = new System.Drawing.Point(283, 128);
+            this.CmdRechts.Name = "CmdRechts";
+            this.CmdRechts.Size = new System.Drawing.Size(89, 46);
+            this.CmdRechts.TabIndex = 2;
+            this.CmdRechts.Text = ">>";
+            this.CmdRechts.UseVisualStyleBackColor = true;
+            this.CmdRechts.Click += new System.EventHandler(this.CmdRechts_Click);
+            // 
+            // CmdLinks
+            // 
+            this.CmdLinks.Location = new System.Drawing.Point(283, 209);
+            this.CmdLinks.Name = "CmdLinks";
+            this.CmdLinks.Size = new System.Drawing.Size(89, 46);
+            this.CmdLinks.TabIndex = 3;
+            this.CmdLinks.Text = "<<";
+            this.CmdLinks.UseVisualStyleBackColor = true;
+            this.CmdLinks.Click += new System.EventHandler(this.CmdLinks_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(655, 450);
+            this.Controls.Add(this.CmdLinks);
+            this.Controls.Add(this.CmdRechts);
+            this.Controls.Add(this.LstRechts);
+            this.Controls.Add(this.LstLinks);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox LstLinks;
+        private System.Windows.Forms.ListBox LstRechts;
+        private System.Windows.Forms.Button CmdRechts;
+        private System.Windows.Forms.Button CmdLinks;
     }
 }
 
--- UListenFeld/Form1.cs
+++ UListenFeld/Form1.cs
@@ -17,5 +17,48 @@ namespace UListenFeld
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LstLinks.Items.Add("Malta");
+            LstLinks.Items.Add("Zypern");
+            LstLinks.Items.Add("Slowenien");
+            LstLinks.Items.Add("Estland");
+            LstLinks.Items.Add("Rumänien");
+
+            LstRechts.Items.Add("Belgien");
+            LstRechts.Items.Add("Spanien");
+            LstRechts.Items.Add("Italien");
+            LstRechts.Items.Add("Portugal");
+            LstRechts.Items.Add("Dänemark");
+        }
+
+        private void CmdRechts_Click(object sender, EventArgs e)
+        {
+            Move(LstLinks, LstRechts);
+        }
+
+        private void CmdLinks_Click(object sender, EventArgs e)
+        {
+            Move(LstRechts, LstLinks);
+
+        }
+        private void Move(ListBox from, ListBox to)
+        {
+            foreach (string s in from.SelectedItems)
+            {
+                to.Items.Add(s);
+            }
+
+            for (int i = from.SelectedIndices.Count - 1; i >= 0; i--)
+            {
+                int position = from.SelectedIndices[i];
+                from.Items.RemoveAt(position);
+            }
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+
+        }
     }
 }
